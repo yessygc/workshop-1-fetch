74110814b09ad406fdf70e78b36c26cf8bb4913a
yessygc <yessgarciacampos@gmail.com>
2022-01-28 20:52:04 -0600

    Usando la API de internacionalizacion
    Usando la API de internacionalización

--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ const baseUrl = "https://platzi-avo.vercel.app";
 
 const appNode = document.querySelector('#app');
 
+const formaPrice = (price) => {
+
+    const newPrice = new window.Intl.NumberFormat('en-EN', {
+        style: 'currency',
+        currency: "USD",
+    }).format(price)
+
+    return newPrice;
+}
+
+// Intl
+// Forma modena
+
 //web api
 // conectar server
 // promise -> asynk/await
@@ -20,16 +33,19 @@ window
         responseJson.data.forEach((item) => {
         //imagen
         const imagen = document.createElement("img");
+        imagen.className = "h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6";
         // URL de la imagen
         imagen.src = `${baseUrl}${item.image}`;
 
         //title
         const title = document.createElement("h2");
         title.textContent = item.name;
+        title.className = "text-lg";
 
         //price
         const price = document.createElement("div");
-        price.textContent = item.price;
+        price.className = "text-gray-600";
+        price.textContent = formaPrice(item.price);
 
         const container = document.createElement("div")
         container.append(imagen, title, price);
