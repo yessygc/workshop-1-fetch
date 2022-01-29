4948b0a8ddc802b06c3723afc6a91bf9d911d001
yessygc <yessgarciacampos@gmail.com>
2022-01-28 20:19:06 -0600

    Enriqueciendo informacion
    Enriqueciendo información

--- src/index.js
+++ src/index.js
@@ -3,31 +3,39 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-const url = "https://platzi-avo.vercel.app/api/avo";
+const baseUrl = "https://platzi-avo.vercel.app";
+
+const appNode = document.querySelector('#app');
 
 //web api
 // conectar server
-window.fetch(url)
-// proecesar la respuesta y convertirla en JSON
-.then((respuesta) => respuesta.json())
-// JSON -> Data -> Renderizar info. browser
-.then((responseJson) => {
-    const todosLosItems = []
-    responseJson.data.forEach((item) => {
+// promise -> asynk/await
+window
+    .fetch(`${baseUrl}/api/avo`)
+    // proecesar la respuesta y convertirla en JSON
+    .then((respuesta) => respuesta.json())
+    // JSON -> Data -> Renderizar info. browser
+    .then((responseJson) => {
+        const todosLosItems = [];
+        responseJson.data.forEach((item) => {
         //imagen
-        const imagen = document.createElement('img');
+        const imagen = document.createElement("img");
+        // URL de la imagen
+        imagen.src = `${baseUrl}${item.image}`;
 
         //title
         const title = document.createElement("h2");
+        title.textContent = item.name;
 
         //price
         const price = document.createElement("div");
+        price.textContent = item.price;
 
-        const container = document.createElement('div')
+        const container = document.createElement("div")
         container.append(imagen, title, price);
 
         todosLosItems.push(container);
     });
 
-    document.body.append(...todosLosItems);
+    appNode.append(...todosLosItems);
 });
