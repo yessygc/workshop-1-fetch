1ff894e5d83c861ed756c049ee59167c24549da9
yessygc <yessgarciacampos@gmail.com>
2022-01-28 19:39:05 -0600

    Descargando informacion y creando nodos
    Descargando informacion y creando nodos

--- src/index.js
+++ src/index.js
@@ -3,4 +3,31 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const url = "https://platzi-avo.vercel.app/api/avo";
+
+//web api
+// conectar server
+window.fetch(url)
+// proecesar la respuesta y convertirla en JSON
+.then((respuesta) => respuesta.json())
+// JSON -> Data -> Renderizar info. browser
+.then((responseJson) => {
+    const todosLosItems = []
+    responseJson.data.forEach((item) => {
+        //imagen
+        const imagen = document.createElement('img');
+
+        //title
+        const title = document.createElement("h2");
+
+        //price
+        const price = document.createElement("div");
+
+        const container = document.createElement('div')
+        container.append(imagen, title, price);
+
+        todosLosItems.push(container);
+    });
+
+    document.body.append(...todosLosItems);
+});
